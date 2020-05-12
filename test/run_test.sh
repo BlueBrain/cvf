de8e427800f1240433ed9e3d8e5e4d62c417bb5f
Ioannis Magkanaris <ioannis.magkanaris@epfl.ch>
2020-05-12 20:10:20 +0200

    FIx the script for bb5

--- test/run_test.sh
+++ test/run_test.sh
@@ -2,16 +2,22 @@
 
 set -ex
 
-spack install coreneuron+nmodl+sympy@develop neuron@develop
+#spack install coreneuron+nmodl+sympy@develop ^nmodl@develop
+# Install coreneuron without nmodl to avoid error with solve method
+spack install coreneuron@develop
+
+spack install neuron@develop
 
 spack module tcl refresh --y --latest coreneuron
 spack module tcl refresh --y --latest neuron
 
 module av coreneuron neuron
 
-module load coreneuron/develop-parallel neuron/develop-parallel
+module load coreneuron/develop neuron/develop
 
 module list
+
+rm -rf x86_64 enginemech.o
 
 # Remove svclmp.mod from mod to avoid redefinition of SEClamp in Neuron
 mv mod/svclmp.mod .
@@ -22,6 +28,9 @@ nrnivmodl mod
 mv svclmp.mod mod
 
 nrnivmodl-core mod
+
+# For BB%
+unset PMI_RANK
 
 # Run script that launches CoreNEURON with SEClamp call
 ./x86_64/special -python neuron_direct.py
