7b73e2b9369e4e557d84585d37a1dfd13cf99e19
Ioannis Magkanaris <ioannis.magkanaris@epfl.ch>
2021-08-25 15:37:04 +0200

    Checkout specific common branch for neocortex

--- .jenkins/get_mod_files.sh
+++ .jenkins/get_mod_files.sh
@@ -4,6 +4,7 @@ source ${JENKINS_DIR:-.}/_env_setup.sh
 set -ex
 
 git clone --recursive ssh://bbpcode.epfl.ch/sim/models/neocortex mod/neocortex
+pushd mod/neocortex/common && git checkout sandbox/magkanar/psolve_direct && popd
 git clone --recursive ssh://bbpcode.epfl.ch/sim/models/hippocampus mod/hippocampus
 git clone --recursive ssh://bbpcode.epfl.ch/sim/models/mousify mod/mousify
 git clone --recursive ssh://bbpcode.epfl.ch/sim/models/thalamus mod/thalamus
